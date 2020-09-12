dad77ad167e285ac7b05a6043bb1e26910486010
yizhi <ian8703031919@gmail.com>
2020-09-12 17:16:18 +0800

    1234

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   server: {
-    port: 8000, // default: 3000
+    port: process.env.PORT || 8000, // default: 3000
     host: '0.0.0.0', // default: localhost,
     timing: false
   },
