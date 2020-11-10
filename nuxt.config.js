010864ce3344a3faaea234db23d0b002ce11540f
yizhi <ian8703031919@gmail.com>
2020-11-10 16:20:05 +0800

    123

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,8 +21,8 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   server: {
-    port: 8887, // default: 3000
-    host: '0.0.0.0', // default: localhost,
+    port: process.env.port || 8887, // default: 3000
+    // host: '0.0.0.0', // default: localhost,
     timing: false
   },
   head: {
@@ -92,7 +92,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'http://localhost:5001'
+    baseURL: process.env.PORT || 'http://localhost:5001'
     // withCredentials: true
   },
   /*
