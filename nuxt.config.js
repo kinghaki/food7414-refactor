28dc9ba69a2257e25bf2fec5ba4574fb9a09cf59
yizhi <ian8703031919@gmail.com>
2020-11-13 11:59:01 +0800

    latestss

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Acme|Nunito&display=swap' }
     ]
   },
   /*
